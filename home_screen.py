a26270bb0571c8f3f24ba314a028510b588addad
aaronheil <heil.aaron@gmx.de>
2024-02-05 16:31:00 +0100

    Version 2.0.5 (PROD - frontend configurations - buttons responsive)

--- home_screen.py
+++ home_screen.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import simpledialog
+from tkinter import messagebox
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from main import User, Leaderboard, get_db_session
+from main import choose_quiz
+from main import get_db_session, Leaderboard, User
+import datetime
 from PIL import Image, ImageTk
-from quiz_screen import start_quiz
 import pygame
 
 
@@ -19,6 +21,19 @@ is_music_playing = True
 # Globale Variablen
 dropdown_list = None
 dropdown_var = None
+
+# Globale Variablen für das Quizspiel
+score = 0
+question_count = 0
+house = ""
+in_tiebreaker_round = False
+bot_score_labels = {}
+score_label = None
+result_label = None
+NUM_OPTIONS = 4
+NUM_QUESTIONS = 10
+POINTS_PER_ANSWER = 10
+bots = {}
 
 
 # Datenbankmodell
@@ -140,6 +155,226 @@ def on_enter_pressed(event):
     else:
         tk.messagebox.showwarning("Warnung", "Kein Benutzer ausgewählt.")
 
+def start_quiz(house_name=None, username=''):
+    global score_label, result_label, score, house, bot_score_labels, bots, bot_scores_frame, quiz_frame, question_count, options, user_house, quiz_background_image, quiz_frame
+
+    # Setze das ausgewählte Haus als das aktuelle Haus
+    if house_name:
+        house = house_name
+        user_house = house_name
+
+        # Entfernen des ausgewählten Hauses aus den Bots
+        if house in bots:
+            del bots[house]  # Entfernt das ausgewählte Haus aus den Bots
+
+            # Erstellen des Frames für Bot-Scores, ohne vertikale Ausdehnung
+            if bot_scores_frame is None:
+                bot_scores_frame = tk.Frame(quiz_frame, width=330)  # Beschränkung der Breite
+
+                # Erstellung einer Überschrift
+                header_label = tk.Label(bot_scores_frame, text="Haeuser-Scores", bg="#f5deb3",
+                                        font=("Harry P", 40))
+                header_label.pack()  # Packen der Überschrift im bot_scores_frame
+
+                # Packen des bot_scores_frame mit Überschrift
+                bot_scores_frame.pack(side='left', anchor='n')
+
+            # hogwarts bots
+            for name in bots.keys():
+                bot_score_labels[name] = tk.Label(bot_scores_frame, text=f"{name}: 0", bg="#f5deb3",
+                                                  font=("Arial", 19))
+                bot_score_labels[name].pack()  # Labels werden jetzt untereinander angeordnet
+
+    # Erstellen des Frames für Quiz-Widgets
+    if quiz_frame is None:
+        quiz_frame = tk.Frame(quiz_frame, width=330)  # Beschränkung der Breite
+
+        bg_image = Image.open(r"C:\Users\aaron\Desktop\HPQ_IU_Material\pictures\prod_perg_1.png")
+        bg_photo = ImageTk.PhotoImage(bg_image)
+
+        # Erstellen eines Labels oder Canvas, um das Bild zu platzieren
+        background_label = tk.Label(quiz_frame, image=bg_photo)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        background_label.image = bg_photo
+
+        # Erstellen eines Labels für die Überschrift innerhalb des quiz_widget_frame
+        quiz_widget_label = tk.Label(quiz_frame, text="Quiz", bg="#f5deb3",
+                                     font=("Harry P", 40))
+        quiz_widget_label.pack()  # Packen der Überschrift im quiz_widget_frame
+
+        # Packen des quiz_widget_frame mit Überschrift
+        quiz_frame.pack(side='left', anchor='n')
+
+    if quiz_background_image is None:
+        # Laden des Hintergrundbildes
+        quiz_background_image = ImageTk.PhotoImage(
+            file=r"C:\Users\aaron\Desktop\HPQ_IU_Material\pictures\prod_perg_1.png")
+        # Nachdem alle anderen Widgets hinzugefügt wurden
+        quiz_background_label = tk.Label(quiz_frame, image=quiz_background_image)
+        quiz_background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        quiz_background_label.lower()
+
+        # Lade die nächste Frage und zeige sie an
+    if question_count < NUM_QUESTIONS:
+        question, options, correct_answer = choose_quiz()
+        question_label = tk.Label(quiz_frame, text=question, bg='white', font=("Arial", 14, "bold"))
+        question_label.pack()
+
+        for i, option in enumerate(options, 1):
+            button = tk.Button(quiz_frame, text=option, bg='white', font=("Arial", 14))
+            button.pack()
+            button.configure(command=lambda b=button, o=option: check_answer(b, o, correct_answer, options, username))
+
+        # Zerstöre score_label und result_label, wenn sie existieren, und erstelle sie neu
+        if score_label is not None:
+            score_label.destroy()
+        if result_label is not None:
+            result_label.destroy()
+
+        score_label = tk.Label(bot_scores_frame, text=f"{user_house} ({username}): {score}", bg="#f5deb3",
+                               font=("Arial", 19))
+        score_label.pack()
+        result_label = tk.Label(bot_scores_frame, text="", bg="#f5deb3", font=("Arial", 19))
+        result_label.pack()
+    else:
+        end_quiz(username)
+
+def check_answer(button, user_answer, correct_answer, options, username):
+    global score, question_count, bots, in_tiebreaker_round
+
+    if user_answer == correct_answer:
+        button.configure(bg='green')
+        result_label.config(text="Richtige Antwort!", fg='green')
+        score += POINTS_PER_ANSWER  # Erhöht die Punktzahl
+    else:
+        button.configure(bg='red')
+        result_label.config(text="Falsche Antwort.", fg='red')
+
+    update_bots_and_scores(options, correct_answer)  # Aktualisieren Sie die Bots und deren Scores
+
+    question_count += 1  # Erhöht die Anzahl der gestellten Fragen
+    if question_count < NUM_QUESTIONS:
+        quiz_window.after(300, lambda: start_quiz(house_name=house, username=username))  # Weitergabe des Benutzernamens
+    else:
+        end_quiz(username)  # Weitergabe des Benutzernamens
+
+    # Prüfe, ob alle Fragen beantwortet wurden oder ob wir uns in einem Tiebreaker befinden
+    if question_count < NUM_QUESTIONS or in_tiebreaker_round:
+        # Lade die nächste Frage
+        quiz_window.after(300, lambda: start_quiz(house_name=house, username=username))
+    else:
+        # Überprüfe auf Unentschieden nur am Ende des normalen Spiels
+         if not in_tiebreaker_round:
+            max_bot_score = max(bot.score for bot in bots.values())
+            if score == max_bot_score:
+                # Unentschieden detektiert, starte zusätzliche Runden
+                in_tiebreaker_round = True
+                question_count = 0  # Setze die Fragezahl zurück für die zusätzlichen Runden
+                quiz_window.after(300, lambda: start_quiz(house_name=house, username=username))
+            else:
+                # Kein Unentschieden, Spiel endet
+                end_quiz(username)
+
+def update_bots_and_scores(options, correct_answer):
+    global bots, bot_score_labels
+
+    # Aktualisieren Sie die Antworten und die Scores der Bots
+    for bot_house, bot in bots.items():
+        bot_choice = bot.choose_answer(options)
+        bot.update_score(bot_choice == correct_answer)
+
+        # Aktualisieren Sie die Labels der Bot-Scores
+        bot_score_labels[bot_house].config(text=f"{bot_house}: {bot.score}")
+
+
+def quit_quiz():
+    global quiz_window, score, question_count
+    score = 0
+    question_count = 0
+
+    if quiz_window is not None:
+        quiz_window.destroy()
+        quiz_window = None
+
+    #open_login_window()
+
+def end_quiz(username=''):
+    global score, house, in_tiebreaker_round, bots, bot_score_labels, question_count
+
+    if in_tiebreaker_round:
+        max_bot_score = max(bot.score for bot in bots.values())
+        if score > max_bot_score:
+            # Spieler hat gewonnen, Spiel endet
+            in_tiebreaker_round = False
+            messagebox.showinfo("Spielende", f"Herzlichen Glückwunsch {username}, du hast gewonnen!")
+        elif score == max_bot_score:
+            # Unentschieden besteht weiterhin, starte weitere zusätzliche Runden
+            in_tiebreaker_round = True
+            question_count = 0  # Setze die Fragezahl zurück für die zusätzlichen Runden
+            messagebox.showinfo("Unentschieden", "Das Spiel ist unentschieden, wir starten zusätzliche Runden!")
+            quiz_window.after(300, lambda: start_quiz(house_name=house, username=username))
+            return  # Verhindere die Ausführung des restlichen Codes, da das Spiel fortgesetzt wird
+        else:
+            # Ein Bot hat gewonnen, Spiel endet
+            in_tiebreaker_round = False
+            winning_bot = max(bots.items(), key=lambda bot: bot[1].score)[0]
+            messagebox.showinfo("Spielende", f"{winning_bot} hat gewonnen. Viel Glück beim nächsten Mal, {username}!")
+    elif not in_tiebreaker_round and question_count == NUM_QUESTIONS:
+        # Normaler Spielabschluss, wenn nicht in zusätzlichen Runden
+        # Prüfe auf Unentschieden am regulären Spielende
+        max_bot_score = max(bot.score for bot in bots.values())
+        if score > max_bot_score:
+            messagebox.showinfo("Spielende", f"Herzlichen Glückwunsch {username}, du hast gewonnen!")
+        elif score == max_bot_score:
+            # Unentschieden detektiert, starte zusätzliche Runden
+            in_tiebreaker_round = True
+            question_count = 0  # Setze die Fragezahl zurück für die zusätzlichen Runden
+            messagebox.showinfo("Unentschieden", "Das Spiel ist unentschieden, wir starten zusätzliche Runden!")
+            quiz_window.after(300, lambda: start_quiz(house_name=house, username=username))
+            return  # Verhindere die Ausführung des restlichen Codes, da das Spiel fortgesetzt wird
+        else:
+            winning_bot = max(bots.items(), key=lambda bot: bot[1].score)[0]
+            messagebox.showinfo("Spielende", f"{winning_bot} hat gewonnen. Viel Glück beim nächsten Mal, {username}!")
+
+    # Entferne alle vorhandenen Widgets
+    for widget in quiz_window.winfo_children():
+        widget.destroy()
+
+    # Zeige das Ergebnis an
+    end_label = tk.Label(quiz_window, text=f"Du hast das Quiz beendet, {username}. Deine Punktzahl ist {score}.", bg='light grey')
+    end_label.pack()
+
+    # Buttons zum Speichern und Abbrechen
+    save_button = tk.Button(quiz_window, text="Ergebnis speichern", command=lambda: save_result(username))
+    save_button.pack()
+
+    cancel_button = tk.Button(quiz_window, text="Abbrechen", command=quit_quiz)
+    cancel_button.pack()
+
+
+
+def save_result(username):
+    global score, house
+    # Erfasse das aktuelle Datum und die Uhrzeit
+    current_time = datetime.datetime.now()
+
+    session = get_db_session()
+    user = session.query(User).filter_by(username=username).first()
+
+    if user is None:
+        # Wenn der Benutzer nicht existiert, fügen Sie ihn hinzu
+        user = User(username=username)
+        session.add(user)
+        session.commit()
+
+    # Neuen Leaderboard-Eintrag erstellen
+    new_entry = Leaderboard(user_id=user.id, house=house, score=score, played_on=current_time)
+    session.add(new_entry)
+    session.commit()
+
+    messagebox.showinfo('Erfolg', 'Dein Ergebnis wurde gespeichert.')
+    quit_quiz()
+
 
 def show_leaderboard(tab):
     style = ttk.Style()
@@ -215,10 +450,13 @@ def open_home_screen():
     nav_frame = tk.Frame(home_window, bg='#343a40')
     nav_frame.pack(side='top', fill='x')  # Oben im Hauptfenster anordnen
 
+    # Grid-Layout für die Platzierung von Frames
 
-    # Container für Frames
+    # Verwenden Sie Grid-Layout für die Platzierung von Frames und Buttons
     frame_container = tk.Frame(home_window)
-    frame_container.pack(fill='both', expand=True, side='bottom')  # Füllt den restlichen Raum, aber unten
+    frame_container.pack(fill='both', expand=True, side='bottom')
+    frame_container.grid_columnconfigure(0, weight=1)
+    frame_container.grid_rowconfigure(0, weight=1)
 
     # Einzelne Frames mit dunkelgrauer Hintergrundfarbe
     home_frame = tk.Frame(frame_container, bg='#343a40')
@@ -226,9 +464,9 @@ def open_home_screen():
     erfolge_frame = tk.Frame(frame_container, bg='#343a40')
     statistik_frame = tk.Frame(frame_container, bg='#343a40')
 
-    # Alle Frames im selben Raum stapeln
     for frame in [home_frame, quiz_frame, erfolge_frame, statistik_frame]:
-        frame.place(x=0, y=0, relwidth=1, relheight=1)
+        frame.grid(row=0, column=0, sticky='nsew')
+
 
     # Globale Variable für den aktuell ausgewählten Button
     active_button = None
@@ -236,19 +474,23 @@ def open_home_screen():
     # Benutzernamen abrufen oder erstellen
     username = get_or_create_username()
 
+
     # Neuer Frame für Begrüßung und Aktionen-Button
     top_frame = tk.Frame(home_frame, bg='#343a40')
-    top_frame.pack(fill='x', pady=10)
+    top_frame.grid(row=0, column=0, sticky='nsew', padx=20, pady=10)
 
-    # Inhalte zu Home Frame hinzufügen
-    welcome_label = tk.Label(top_frame, text=f"Hallo {username}", font=("Harry P", 30), relief=tk.RAISED, width=20)
-    welcome_label.pack(side='left', padx=(950, 0), pady=10)
+    welcome_label = tk.Label(top_frame, text=f"Hallo {username}", font=("Harry P", 30), relief=tk.RAISED)
+    welcome_label.grid(row=0, column=0, sticky='w', padx=10, pady=10)
 
     # Aktionen-Button direkt im top_frame ohne zusätzlichen Container
     actions_button = tk.Menubutton(top_frame, text="Aktionen", font=("Harry P", 30), relief=tk.RAISED, width=20)
     actions_menu = tk.Menu(actions_button, tearoff=0)
     actions_button["menu"] = actions_menu
-    actions_button.pack(side='left', padx=(450, 0), pady=10)
+    actions_button.grid(row=0, column=1, sticky='w', padx=10, pady=10)
+
+    # Erstellen des Quiz-Frames innerhalb von frame_container
+    quiz_frame = tk.Frame(frame_container, bg='#343a40')
+    quiz_frame.place(x=0, y=0, relwidth=1, relheight=1)
 
     # Funktion zum Anzeigen der Dropdown-Suchleiste
     def show_dropdown():
@@ -297,9 +539,11 @@ def open_home_screen():
     actions_menu.add_command(label="Musik Ein / Aus", command=toggle_music, font=("Arial", 16), background='white',
                              foreground='black')
 
-    # Container-Frame für die Buttons
+
     button_frame = tk.Frame(home_frame, bg='#343a40')
-    button_frame.pack(expand=True, padx=20, pady=20)
+    button_frame.grid(row=1, column=0, sticky='nsew', padx=20, pady=20)
+    button_frame.grid_columnconfigure([0, 1, 2, 3], weight=1)
+    button_frame.grid_rowconfigure(0, weight=1)
 
     # Funktionen zum Laden und Skalieren der Bilder
     def load_and_resize_image(image_path, size=(400, 500)):
@@ -311,22 +555,22 @@ def open_home_screen():
     image_gryffindor = load_and_resize_image(r"C:\Users\aaron\Desktop\HPQ_IU_Material\pictures\gryffindor.png")
     btn_gryffindor = tk.Button(button_frame, image=image_gryffindor,
                                command=lambda: on_house_select('Gryffindor', current_username))
-    btn_gryffindor.pack(side='left', fill='both', expand=True, padx=200, pady=30)  # Abstand zwischen den Buttons
+    btn_gryffindor.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)  # Abstand zwischen den Buttons
 
     image_slytherin = load_and_resize_image(r"C:\Users\aaron\Desktop\HPQ_IU_Material\pictures\slytherin.png")
     btn_slytherin = tk.Button(button_frame, image=image_slytherin,
                               command=lambda: on_house_select('Slytherin', current_username))
-    btn_slytherin.pack(side='left', fill='both', expand=True, padx=200, pady=30)  # Abstand zwischen den Buttons
+    btn_slytherin.grid(row=0, column=1, sticky='nsew', padx=10, pady=10)  # Abstand zwischen den Buttons
 
     image_hufflepuff = load_and_resize_image(r"C:\Users\aaron\Desktop\HPQ_IU_Material\pictures\hufflepuff.png")
     btn_hufflepuff = tk.Button(button_frame, image=image_hufflepuff,
                                command=lambda: on_house_select('Hufflepuff', current_username))
-    btn_hufflepuff.pack(side='left', fill='both', expand=True, padx=200, pady=30)  # Abstand zwischen den Buttons
+    btn_hufflepuff.grid(row=0, column=2, sticky='nsew', padx=10, pady=10)  # Abstand zwischen den Buttons
 
     image_ravenclaw = load_and_resize_image(r"C:\Users\aaron\Desktop\HPQ_IU_Material\pictures\ravenclaw.png")
     btn_ravenclaw = tk.Button(button_frame, image=image_ravenclaw,
                               command=lambda: on_house_select('Ravenclaw', current_username))
-    btn_ravenclaw.pack(side='left', fill='both', expand=True, padx=200, pady=30)  # Abstand zwischen den Buttons
+    btn_ravenclaw.grid(row=0, column=3, sticky='nsew', padx=10, pady=10)  # Abstand zwischen den Buttons
 
     # Halten Sie die Bildreferenzen, um die automatische Bereinigung durch Garbage Collector zu verhindern
     button_frame.image_gryffindor = image_gryffindor
@@ -334,11 +578,15 @@ def open_home_screen():
     button_frame.image_hufflepuff = image_hufflepuff
     button_frame.image_ravenclaw = image_ravenclaw
 
-
     def on_house_select(house_name, username):
+        if not username:  # Überprüfen, ob der Benutzername leer ist
+            messagebox.showinfo("Fehler", "Bitte erst ein Haus auswählen.")
+            return
         print(f"{house_name} ausgewählt, Benutzer: {username}")
-        # Stellen Sie sicher, dass die Funktion start_quiz verfügbar ist und korrekt implementiert wird
-        home_window.destroy()
+        # Hier, anstatt das home_window zu zerstören, einfach den quiz_frame in den Vordergrund bringen
+        switch_frame(quiz_frame)
+        update_active_button(quiz_button)
+        # Starte das Quiz mit dem ausgewählten Haus und Benutzernamen
         start_quiz(house_name, username)
 
     def update_active_button(new_active_button):
